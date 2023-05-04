e18d72798cf1364c06a57aadd18300e7a6210418
Dayana <dayana_marcia@hotmail.com>
2023-05-03 22:39:32 -0300

    Criando service para autenticação de token de sessão

---
+++ src/modules/users/services/CreateSessionsService.ts
@@ -0,0 +1,34 @@
+import AppError from '@shared/errors/AppError';
+import { getCustomRepository } from 'typeorm';
+import User from '../typeorm/entities/User';
+import { UsersRepository } from '../typeorm/repositories/UsersRepository';
+import { compare, hash } from 'bcryptjs';
+
+interface IRequest {
+	email: string;
+	password: string;
+}
+
+interface IResponse {
+	user: User;
+}
+class CreateSessionsService {
+	public async execute({ email, password }: IRequest): Promise<User> {
+		const usersRepository = getCustomRepository(UsersRepository);
+		const user = await usersRepository.findByEmail(email);
+
+		if (!user) {
+			throw new AppError('Incorrect e-mail/password combination', 401);
+		}
+
+		const passwordConfirmed = await compare(password, user.password)
+
+		if (!passwordConfirmed) {
+			throw new AppError('Incorrect e-mail/password combination', 401);
+		}
+
+		return user;
+	}
+}
+
+export default CreateSessionsService;
